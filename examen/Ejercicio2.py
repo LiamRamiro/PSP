5372b546af81bd80f523b89989c346f37efe07d8
LiamRamiro <ramiro.emlia22@zaragoza.salesianos.edu>
2023-11-28 09:58:18 +0100

    examen ev1 commit 2

--- examen/Ejercicio2.py
+++ examen/Ejercicio2.py
@@ -8,12 +8,22 @@ file_name = os.path.join(tempfile.gettempdir(), os.urandom(24).hex())
 
 def code(name):
     time.sleep(10)
-    data_to_write = f"codigo limpio fue escrito por {name}\n"
-
     with file_lock:
         with open(file_name, 'a') as f:
             print(f"Guardando en {file_name}")
-            f.write(data_to_write)
-    subprocess.run(["ping", "google.com"])
+            f.write("codigo limpio fue escrito por "+str(name)) 
+        subprocess.run(["ping","-c","4","google.com"])
 
+def multihilo():
+    threads = []
+    for i in range(4):  
+        thread = threading.Thread(target=code, args=(f"Thread-{i+1}",))
+        thread.start()
+        threads.append(thread)
 
+    for thread in threads:
+        thread.join()
+
+if __name__ == "__main__":
+    file_lock = threading.Lock()
+    multihilo()
